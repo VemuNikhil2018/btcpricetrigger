a99c2591a1445c00e38407572c2a83f7a7865267
VemuNikhil2018 <67622048+VemuNikhil2018@users.noreply.github.com>
2021-09-10 20:59:52 +0530

    BTC Price Trigger.py
    This intelligent program asks for a pre-defined Bitcoin price, and when the real Bitcoin reaches the price, it sends an email to the user.
    I called it intelligent, because:
    1. It checks the regex of the entered mail for its validity.
    2. It checks if the entered email really exists on the internet. So if it doesn't, fake emails won't mess up the mailing list.
    3. If the entered mail already exists in the mailing list, it's never added twice. So, multiple emails are avoided.
    4. It constantly checks the price of Bitcoin with the minimum time frequency possible. So, immediate fluctuations are caught too.
    5. It maintain the history of all the requests so the list will be available on-demand with the exact time and dates.
    6. Invalid inputs are detected automatically.

--- btcpricetrigger.py
+++ btcpricetrigger.py
@@ -1,1 +1,70 @@
-
+import requests
+import json
+import smtplib
+import urllib
+import urllib.request
+import re
+t=requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+t=t.json()
+btcprice=t["bitcoin"]["usd"]
+print("The current price of BTC is "+str(btcprice))
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("btcalerttrigger", "BT(@lerttrigger")
+def settriggers():
+    a=str(input("Do you want to set an alert? Enter Y/N: "))
+    if(a=='Y'):
+        target=input("Enter your target price: ")
+        send=urllib.request.urlopen('https://api.thingspeak.com/update?api_key=YE4BDPOOVTOCAPMS&field1='+target)
+        emails=[]
+        nemails=int(input("Enter the number of emails: "))
+        print("Enter the emails one by one:")
+        n=0
+        while(n!=nemails):
+            email=str(input(str(n+1)+'. '))
+            regex=r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            if(re.fullmatch(regex,email)==None):
+                print("Not a valid email. Please try again.")
+            else:
+                exists=requests.get("https://isitarealemail.com/api/email/validate",params = {'email': email})
+                exists=exists.json()['status']
+                if(exists=="valid" and emails.count(email)==0):
+                    print("Email is valid. Let's proceed.")
+                    emails.append(email)
+                    n+=1
+                else:
+                    if(emails.count(email)==1):
+                        print("Email already exists. Skip this.")
+                        n+1
+                    else:
+                        print("Email not yet exists. Please check again.")
+        print("All emails entered. Checking Bitcoin price...")
+        while True:
+            t=requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+            t=t.json()
+            btcprice=t["bitcoin"]["usd"]
+            if(btcprice==target):
+                for dest in emails:
+                    message = "Hello! The Bitcoin reached your target price "+str(target)+"!"
+                    s.sendmail("btcalerttrigger@gmail.com",dest,message)
+                s.quit()
+                return("All mails sent.")
+    if(a=='N'):
+        b=str(input("Do you want to see your trigger history? Enter Y/N: "))
+        if(b=='Y'):
+            a=requests.get("https://thingspeak.com/channels/1502719/field/1")
+            a=a.json()['feeds']
+            for i in a:
+                timestamp=i['created_at']
+                value=i['field1']
+                indexT=timestamp.index('T')
+                date=timestamp[:indexT]
+                time=timestamp[indexT+1:][:-1]
+                print("Target of "+str(value)+" added on date "+str(date)+" at time "+str(time)+" GMT.")
+            return("Done.")
+        if(b=='N'):
+            return("Thanks for visiting our app!")
+    else:
+        print("Invalid input. Please try again.")
+        return settriggers()
+print(settriggers())
